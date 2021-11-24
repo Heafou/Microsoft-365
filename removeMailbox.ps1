5b12a12211549fc9b6c52db91e199c1df08b1cb4
Heafou <62443201+Heafou@users.noreply.github.com>
2021-11-24 14:36:17 +0100

    Add files via upload

---
+++ removeMailbox.ps1
@@ -0,0 +1,31 @@
+﻿<#
+Import users from txt file to remove their online mailbox in a hybrid deployment using licence Office 365 E3
+Plan may change when using other O365 licence than E3
+Just modify the param parameters to use it on your environment  
+#>
+
+Import-Module ExchangeOnlineManagement
+Import-Module MSOnline
+
+$param = @{
+    username = "Enter UserPrincipalName"
+    filePath = "Enter Path like c:\temp\"
+    fileName = "Enter name like users.txt"
+    plan = "EXCHANGE_S_ENTERPRISE"
+}
+
+
+$credential=Get-Credential -Credential $param.username
+Connect-ExchangeOnline -Credential $credential
+Connect-MsolService -Credential $credential
+
+$users = Get-Content  $($param.filePath + $param.fileName)
+
+$SkuIdName=Get-MsolAccountSku |where {$_.AccountSkuId -like "*ENTERPRISEPACK"} | select AccountSkuId 
+$SkuId= $SkuIdName.AccountSkuId
+$LO = New-MsolLicenseOptions -AccountSkuId $SkuId -DisabledPlans $param.plan
+ 
+foreach($user in $users){ 
+Set-MsolUserLicense -UserPrincipalName $user -LicenseOptions $LO
+Set-User $user -PermanentlyClearPreviousMailboxInfo -confirm:$false
+}
